286214168ffa0b5d755e3c73756ee4aafe0bf066
Alejandro Vicario <a.vicarioe@gmail.com>
2018-02-28 13:30:57 +0100

    PWM working

--- src/main.c
+++ src/main.c
@@ -5,8 +5,6 @@
 
 
 
-
-
 // Mensaje de inicio
 #define STRING_HEADER "-- Blink LED Example --\r\n" \
 		"-- "BOARD_NAME" --\r\n" \
@@ -82,6 +80,34 @@ void TC0_Handler(void) {
 	}
 }
 
+static void pwm_config(void){
+	pwm_channel_t pwm_channel_instance;
+	pmc_enable_periph_clk(ID_PWM);
+	pwm_channel_disable(PWM, PWM_CHANNEL_0);
+	pwm_clock_t clock_setting = {
+	    .ul_clka = 1000 * 200,
+	    .ul_clkb = 0,
+	    .ul_mck = sysclk_get_cpu_hz()
+	};
+	pwm_init(PWM, &clock_setting);
+		/* Initialize PWM channel for LED0 */
+		/* Period is left-aligned */
+		pwm_channel_instance.alignment = PWM_ALIGN_LEFT;
+		/* Output waveform starts at a low level */
+		pwm_channel_instance.polarity = PWM_HIGH;
+		/* Use PWM clock A as source clock */
+		pwm_channel_instance.ul_prescaler = PWM_CMR_CPRE_CLKA;
+		/* Period value of output waveform */
+		pwm_channel_instance.ul_period = 200;
+		/* Duty cycle value of output waveform */
+		pwm_channel_instance.ul_duty = 50;
+		pwm_channel_instance.channel = PWM_CHANNEL_0;
+	pwm_channel_init(PWM, &pwm_channel_instance);
+
+	//pwm_channel_enable_interrupt(PWM, PWM_CHANNEL_0, 0);
+	pwm_channel_enable(PWM, PWM_CHANNEL_0);
+}
+
 
 /**
  * Application entry point
@@ -89,6 +115,15 @@ void TC0_Handler(void) {
 int main(void) {
 	// Rutina de configuración
 	due_init();
+
+	ioport_init();
+
+	pio_configure_pin(PIO_PC3_IDX, 0 |
+		PIO_TYPE_PIO_OUTPUT_1 |
+		PIO_DEFAULT);
+	pio_configure_pin(PIO_PC3_IDX, 0 |
+		PIO_PERIPH_B |
+		PIO_DEFAULT);
 
 	// Imprime el mensaje de inicio
 	puts(STRING_HEADER);
@@ -98,8 +133,12 @@ int main(void) {
 	// Establece el pin 27 como salida
 	pio_set_output(PIOB, PIO_PB27, LOW, DISABLE, ENABLE);
 
+
+
 	// Rutina de configuración del timer
 	tc_config();
+
+	pwm_config();
 
 	// Infinite loop
 	while (1) {
